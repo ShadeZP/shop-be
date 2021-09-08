064e7d1d52ffb690dac5018d602808f9acd07ff8
Evgeny Vasilko <ShadeZP@gmail.com>
2021-09-08 22:22:02 +0300

    initial commit

--- product-service/src/functions/mock.ts
+++ product-service/src/functions/mock.ts
@@ -5,7 +5,7 @@ export const mockProducts: IProduct[] = [
     count: 4,
     description: 'Short Product Description1',
     id: '7567ec4b-b10c-48c5-9345-fc73c48a80aa',
-    price: 2.4,
+    price: 2.3,
     title: 'ProductOne'
   },
   {
